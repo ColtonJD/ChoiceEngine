89db9cc55de73c53088f6365c7cc8862f659fc7d
Cj <cjdale1991@gmail.com>
2018-08-19 16:51:36 -0700

    now handling finding event coordinates and moving messages to them

--- ChoiceMessage.js
+++ ChoiceMessage.js
@@ -14,11 +14,15 @@ ChoiceEngine.Message = ChoiceEngine.Message || {};
 //  New Function Descriptions
 //-----------------------------------------------------------------------------
  /*
- * Window_Message.prototype.customPosition()
- * Desc: Used for determing if a target was set, and the locations of the target.
- * Returns: 
- * 1. eventId: TheId of the targeted event determined by parsing $gameMessage text 
- * for \target[eventId].
+ * Function: Window_Message.prototype.target()
+ * Desc: Checks if a target has been identified in the string. If it has it 
+ * returns a object containing target details
+ * Returns:
+ * target{
+ *  found: Boolean,
+ *  x: x position of target adjusted for offset,
+ *  y: y position of taget adjusted for offset, 
+ * }
  * 
  * 
  */
@@ -34,6 +38,13 @@ ChoiceEngine.Message = ChoiceEngine.Message || {};
  * @desc Graphic that connects to the speech bubble. 
  * @default Pointer1
  *
+ * @param Target Offset X
+ * @desc X Offset for Targeted Messages
+ * @default 0
+ * 
+ * @param Target Offset Y
+ * @desc Y Offset for Targeted Messages
+ * @default 0
  */
 ChoiceEngine.Message.Params = PluginManager.parameters('ChoiceMessage');
 
@@ -42,10 +53,25 @@ ChoiceEngine.Message.Params = PluginManager.parameters('ChoiceMessage');
 //-----------------------------------------------------------------------------
 //  Window_Base
 //-----------------------------------------------------------------------------
-ChoiceEngine.Message._Base_converEscapeCharacters = Window_Base.prototype.convertEscapeCharacters;
-Window_Base.prototype.convertEscapeCharacters = function(text) {
-    ChoiceEngine.Message._Base_converEscapeCharacters.call(this, text);
 
+Window_Base.prototype.convertEscapeCharacters = function(text) {
+    text = text.replace(/\\/g, '\x1b');
+    text = text.replace(/\x1b\x1b/g, '\\');
+    text = text.replace(/\x1bV\[(\d+)\]/gi, function() {
+        return $gameVariables.value(parseInt(arguments[1]));
+    }.bind(this));
+    text = text.replace(/\x1bV\[(\d+)\]/gi, function() {
+        return $gameVariables.value(parseInt(arguments[1]));
+    }.bind(this));
+    text = text.replace(/\x1bN\[(\d+)\]/gi, function() {
+        return this.actorName(parseInt(arguments[1]));
+    }.bind(this));
+    text = text.replace(/\x1bP\[(\d+)\]/gi, function() {
+        return this.partyMemberName(parseInt(arguments[1]));
+    }.bind(this));
+    text = text.replace(/\x1bG/gi, TextManager.currencyUnit);
+    text = text.replace(/\[.*?\]/g, '');
+    return text;
 };
 
 //-----------------------------------------------------------------------------
@@ -55,14 +81,17 @@ Window_Base.prototype.convertEscapeCharacters = function(text) {
 Window_Message.prototype.startMessage = function() {
     this._textState = {};
     this._textState.index = 0;
-
-    var target = this.customPosition();
-
+    var target = new Window_Message.prototype.target();
+    
     this._textState.text = this.convertEscapeCharacters($gameMessage.allText());
-
-    if (target >= 0 && target !== "false"){
-        alert(target)
-    } else{
+    
+    
+    if(target.found == true){
+        this.newPage(this._textState);
+        this.customPlacement(target);
+        this.updateBackground();
+        this.open();
+    }else{
         this.newPage(this._textState);
         this.updatePlacement();
         this.updateBackground();
@@ -70,16 +99,31 @@ Window_Message.prototype.startMessage = function() {
     }
 };
 
-Window_Message.prototype.customPosition = function() {
-    this._eventid = 'false';
-    this._text = $gameMessage.allText();
-    //Accepting text from another method cannot be garunteed to not have escaped characters removed
-    this._text = this._text.replace(/TARGET\[(\d+)\]/gi, function() {
-        this._eventid = Number(arguments[1]);
+Window_Message.prototype.target = function() {
+    this.found = false;
+    var text = $gameMessage.allText();
+
+    text = text.replace(/TARGET\[(\d+)\]/gi, function() {
+        var eventid = arguments[1];
+        if (eventid > 0) {
+        this.found = true; 
+        var ev = $gameMap.event(eventid);
+        this.x = ev.screenX();
+        this.y = ev.screenY();
+        } else{
+            
+        }
         return '';
     }.bind(this));
 
-    return this._eventid;
+    return this;
+};
+
+Window_Message.prototype.customPlacement = function(target) {
+    this._positionType = $gameMessage.positionType();
+    this.y = target.y;
+    this.x = target.x;
+    this._goldWindow.y = this.y > 0 ? 0 : Graphics.boxHeight - this._goldWindow.height;
 };
 
 })();
