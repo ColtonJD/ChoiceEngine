1ec0dade5498ba72f8eba8467e03d585bc03367c
Cj <cjdale1991@gmail.com>
2018-08-20 19:02:12 -0700

    Working positioning offsets

--- ChoiceMessage.js
+++ ChoiceMessage.js
@@ -14,17 +14,23 @@ ChoiceEngine.Message = ChoiceEngine.Message || {};
 //  New Function Descriptions
 //-----------------------------------------------------------------------------
  /*
- * Function: Window_Message.prototype.target()
- * Desc: Checks if a target has been identified in the string. If it has it 
- * returns a object containing target details
+ * Function: Window_Message.prototype.customMessage()
+ * Desc: Checks message for custom settings and returns object with the
+ * flags / values
+ * Matches on:
+ *  /target[target] (target an event or player)
+ *  /fitmessage (Fits message to fit text)
  * Returns:
  * target{
- *  found: Boolean,
- *  x: x position of target adjusted for offset,
- *  y: y position of taget adjusted for offset, 
+ *  found: Boolean, (is there a target)
+ *  "x": Number (x position of target adjusted for offset),
+ *  "y": Number (y position of taget adjusted for offset),
+ *  "fitmsg": Boolean (flag to shrink message) 
  * }
  * 
- * 
+ * Function: Window_Message.prototype.customPlacement()
+ * Desc: Replaces Window_Message.updatePlacement when moving message to   
+ * a custom location
  */
 
 //-----------------------------------------------------------------------------
@@ -44,9 +50,11 @@ ChoiceEngine.Message = ChoiceEngine.Message || {};
  * 
  * @param Target Offset Y
  * @desc Y Offset for Targeted Messages
- * @default 0
+ * @default 150
  */
 ChoiceEngine.Message.Params = PluginManager.parameters('ChoiceMessage');
+ChoiceEngine.Message.Target_Y_Offset = Number(ChoiceEngine.Message.Params['Target Offset Y']);
+ChoiceEngine.Message.Target_X_Offset = Number(ChoiceEngine.Message.Params['Target Offset X']);
 
 (function() {
 
@@ -81,13 +89,19 @@ Window_Base.prototype.convertEscapeCharacters = function(text) {
 Window_Message.prototype.startMessage = function() {
     this._textState = {};
     this._textState.index = 0;
-    var target = new Window_Message.prototype.target();
-    
+    var target = new Window_Message.prototype.customMessage();
+
     this._textState.text = this.convertEscapeCharacters($gameMessage.allText());
     
+    var temp = this.textWidth($gameMessage.text);
+    console.log(temp);
     
     if(target.found == true){
         this.newPage(this._textState);
+        
+        target.x -= (this.width / 2) + ChoiceEngine.Message.Target_X_Offset;
+        target.y -= this.height + ChoiceEngine.Message.Target_Y_Offset; 
+        
         this.customPlacement(target);
         this.updateBackground();
         this.open();
@@ -99,25 +113,45 @@ Window_Message.prototype.startMessage = function() {
     }
 };
 
-Window_Message.prototype.target = function() {
+Window_Message.prototype.customMessage = function() {
     this.found = false;
     var text = $gameMessage.allText();
 
-    text = text.replace(/TARGET\[(\d+)\]/gi, function() {
-        var eventid = arguments[1];
-        if (eventid > 0) {
-        this.found = true; 
-        var ev = $gameMap.event(eventid);
-        this.x = ev.screenX();
-        this.y = ev.screenY();
-        } else{
-            
-        }
+    text = text.replace(/\bTARGET\[(\d+)\]/gi, function() {
+            var eventid = arguments[1];
+            if (eventid > 0) {
+            this.found = true; 
+            var ev = $gameMap.event(eventid);
+            this.x = ev.screenX();
+            this.y = ev.screenY();
+        } else if (eventid == 0){
+            this.found = true; 
+            this.x = $gamePlayer.screenX();
+            this.y = $gamePlayer.screenY();
+        } else {
+
+        };
         return '';
+    }.bind(this));
+    
+    text = text.replace(/\bFITMSG/gi, function() {
+        console.log("fit message found");
+        return ''
     }.bind(this));
 
     return this;
 };
+
+/*Window_Message.prototype.sizeWindow = function() {
+    var textArr = $gameMessage._texts;
+    for (var i = 0; i < textArr.length; i++) {
+        if($gameMessage._texts[i] == ''){
+            textArr.slice(i, i+1);
+        } else {
+            console.log(textArr[i]);
+        }
+    }
+}*/
 
 Window_Message.prototype.customPlacement = function(target) {
     this._positionType = $gameMessage.positionType();
